d91df711565d98fd0cc69871c64288cf66ec0ba7
PablorellanaM <ore20555@uvg.edu.gt>
2024-03-13 23:06:19 -0600

    Test Division

--- lispinterpreter/src/test/java/com/uvg/edu/gt/EvaluatorTest.java
+++ lispinterpreter/src/test/java/com/uvg/edu/gt/EvaluatorTest.java
@@ -27,4 +27,11 @@ public class EvaluatorTest {
         List<Object> expression = Arrays.asList("*", 4, 3);
         assertEquals(12.0, evaluator.eval(expression, evaluator.getGlobalEnvironment()));
     }
+
+    @Test
+    public void testDivision() {
+        Evaluator evaluator = new Evaluator();
+        List<Object> expression = Arrays.asList("/", 8, 2);
+        assertEquals(4.0, evaluator.eval(expression, evaluator.getGlobalEnvironment()));
+    }
 }
